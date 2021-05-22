0caa2d79014c24481b443db547389fe47b2d1cf4
Alvaro Fontalez <alvarofontalez@MBP-de-Alvaro.home>
2021-05-22 14:37:54 +0200

    mi primer commit

---
+++ map-kit-tutorial-2/ViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  map-kit-tutorial-1
+//
+//  Created by Code Pro on 1/28/18.
+//  Copyright © 2018 Code Pro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        PresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    @objc func showMiracle() {
+        let slideVC = OverlayView()
+        slideVC.modalPresentationStyle = .custom
+        slideVC.transitioningDelegate = self
+        self.present(slideVC, animated: true, completion: nil)
+    }
+    
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate: CLLocationCoordinate2D?
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        
+        configureLocationServices()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func configureLocationServices() {
+        locationManager.delegate = self
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
+           beginLocationUpdates(locationManager: locationManager)
+        }
+    }
+    
+    private func beginLocationUpdates(locationManager: CLLocationManager) {
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
+    
+    private func addAnnotations() {
+       
+        
+        let appleParkAnnotation = MKPointAnnotation()
+        appleParkAnnotation.title = "Apple Park"
+        appleParkAnnotation.coordinate = CLLocationCoordinate2D(latitude: 37.332072300 , longitude:-122.011138100)
+        
+        let ortegaParkAnnotation = MKPointAnnotation()
+        ortegaParkAnnotation.title = "Ortega Park"
+        ortegaParkAnnotation.coordinate = CLLocationCoordinate2D(latitude:    37.342226 , longitude: -122.025617)
+        
+        mapView.addAnnotation(appleParkAnnotation)
+        mapView.addAnnotation(ortegaParkAnnotation)
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did get latest location")
+        
+        guard let latestLocation = locations.first else { return }
+        
+        if currentCoordinate == nil {
+            zoomToLatestLocation(with: latestLocation.coordinate)
+            addAnnotations()
+        }
+    
+        currentCoordinate = latestLocation.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+       print("The status changed")
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdates(locationManager: manager)
+        }
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
+        }
+        
+        if let title = annotation.title, title == "Apple Park" {
+            annotationView?.image = UIImage(named: "car")
+        } else if let title = annotation.title, title == "Ortega Park" {
+            annotationView?.image = UIImage(named: "car")
+        } else if annotation === mapView.userLocation {
+            annotationView?.image = UIImage(named: "loc")
+        }
+        
+        annotationView?.canShowCallout = true
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("The annotation was selected: \(String(describing: view.annotation?.title))")
+
+
+/*     func prepare(for segue: UIStoryboardSegue, sender: UILabel) {
+            let detalleScripts = segue.destination as! OverlayView
+            //agregas una variable de tipo tu view controller (hijo)
+                
+            detalleScripts.restauranteTXT.text = "dsfas"
+            //accedes al label de ese view controller y le asignas el valor
+            }*/
+        showMiracle()
+
+        let timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
+            mapView.deselectAnnotation(view.annotation, animated: true)
+            }
+    }
+}
+
